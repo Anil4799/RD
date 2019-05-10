068cea38712a4f9b831501d0b8f2da38368a1669
Nayamtulla_Khan <Nayamtulla_Khan@EPINHYDW1330.princeton.epam.com>
2019-05-10 14:29:33 +0530

    Commit 3: Test Cases

--- src/test/java/com/epam/student/service/TestStudentServiceImpl.java
+++ src/test/java/com/epam/student/service/TestStudentServiceImpl.java
@@ -26,83 +26,42 @@ class TestStudentServiceImpl {
 		assertEquals(true, check);
 	
 	}
-	/*@Test
-	void testget()
-	{ 
-		String firstName=null;
-		String lastName=null;
-		Date dob=null;
-		String email=null;
-		String gender=null;
-		long contactNumber=0;
-		String personalLocation=null;
-		String collegeName=null;
-		String collegeLocation=null;
-		String graduation=null;
-		String graduationSpeciality=null;
-		int yearOfPassedOut=0;
-		int graduationMarks=0;
-		int twelveth=0;
-		int tenth=0;
-		String batchId=null;
-		String employeeType=null;
-		String coreSkill=null;
-		String preferredStudentStream=null;
-		String assignedStream=null;
-		//Date dateOfJoining=null;
-		String mentorName=null;
-		String assignedLocation=null;
-		String relocation=null;
-		String status=null;
-		firstName=bean.getFirstName();
-		lastName=bean.getLastName();
-		dob=bean.getDob();
-		email=bean.getEmail();
-		gender=bean.getGender();
-		contactNumber=bean.getContactNumber();
-		personalLocation=bean.getPersonalLocation();
-		collegeName=bean.getCollegeName();
-		collegeLocation=bean.getCollegeLocation();
-		graduation=bean.getGraduation();
-		graduationSpeciality=bean.getGraduationSpeciality();
-		yearOfPassedOut=bean.getYearOfPassedOut();
-		graduationMarks=bean.getGraduationMarks();
-		twelveth=bean.getTwelveth();
-		tenth=bean.getTenth();
-		batchId=bean.getBatchId();
-		employeeType=bean.getEmployeeType();
-		coreSkill=bean.getCoreSkill();
-		preferredStudentStream=bean.getPreferredStudentStream();
-		assignedStream=bean.getAssignedStream();
-		mentorName=bean.getMentorName();
-		assignedLocation=bean.getAssignedLocation();
-		relocation=bean.getRelocation();
-		status=bean.getStatus();
-		assertNotNull(firstName);
-		assertNotNull(lastName);
-		assertNotNull(email);
-		assertNotNull(gender);
-		assertNotNull(firstName);
-		assertNotNull(contactNumber);
-		assertNotNull(personalLocation);
-		assertNotNull(collegeName);
-		assertNotNull(collegeLocation);
-		assertNotNull(graduation);
-		assertNotNull(graduationSpeciality);
-		assertNotNull(yearOfPassedOut);
-		assertNotNull(graduationMarks);
-		assertNotNull(twelveth);
-		assertNotNull(tenth);
-		assertNotNull(batchId);
-		assertNotNull(employeeType);
-		assertNotNull(coreSkill);
-		assertNotNull(preferredStudentStream);
-		assertNotNull(assignedStream);
-		assertNotNull(mentorName);
-		assertNotNull(assignedLocation);
-		assertNotNull(relocation);
-		assertNotNull(status);		
-		
-	}*/
+	/*
+	 * @Test void testget() { String firstName=null; String lastName=null; Date
+	 * dob=null; String email=null; String gender=null; long contactNumber=0; String
+	 * personalLocation=null; String collegeName=null; String collegeLocation=null;
+	 * String graduation=null; String graduationSpeciality=null; int
+	 * yearOfPassedOut=0; int graduationMarks=0; int twelveth=0; int tenth=0; String
+	 * batchId=null; String employeeType=null; String coreSkill=null; String
+	 * preferredStudentStream=null; String assignedStream=null; //Date
+	 * dateOfJoining=null; String mentorName=null; String assignedLocation=null;
+	 * String relocation=null; String status=null; firstName=bean.getFirstName();
+	 * lastName=bean.getLastName(); dob=bean.getDob(); email=bean.getEmail();
+	 * gender=bean.getGender(); contactNumber=bean.getContactNumber();
+	 * personalLocation=bean.getPersonalLocation();
+	 * collegeName=bean.getCollegeName(); collegeLocation=bean.getCollegeLocation();
+	 * graduation=bean.getGraduation();
+	 * graduationSpeciality=bean.getGraduationSpeciality();
+	 * yearOfPassedOut=bean.getYearOfPassedOut();
+	 * graduationMarks=bean.getGraduationMarks(); twelveth=bean.getTwelveth();
+	 * tenth=bean.getTenth(); batchId=bean.getBatchId();
+	 * employeeType=bean.getEmployeeType(); coreSkill=bean.getCoreSkill();
+	 * preferredStudentStream=bean.getPreferredStudentStream();
+	 * assignedStream=bean.getAssignedStream(); mentorName=bean.getMentorName();
+	 * assignedLocation=bean.getAssignedLocation(); relocation=bean.getRelocation();
+	 * status=bean.getStatus(); assertNotNull(firstName); assertNotNull(lastName);
+	 * assertNotNull(email); assertNotNull(gender); assertNotNull(firstName);
+	 * assertNotNull(contactNumber); assertNotNull(personalLocation);
+	 * assertNotNull(collegeName); assertNotNull(collegeLocation);
+	 * assertNotNull(graduation); assertNotNull(graduationSpeciality);
+	 * assertNotNull(yearOfPassedOut); assertNotNull(graduationMarks);
+	 * assertNotNull(twelveth); assertNotNull(tenth); assertNotNull(batchId);
+	 * assertNotNull(employeeType); assertNotNull(coreSkill);
+	 * assertNotNull(preferredStudentStream); assertNotNull(assignedStream);
+	 * assertNotNull(mentorName); assertNotNull(assignedLocation);
+	 * assertNotNull(relocation); assertNotNull(status);
+	 * 
+	 * }
+	 */
 
 }
