4221e49a49ec6c000a9f4308dfb2c8adf735ece8
Srikanth_Tigapuram <Srikanth_Tigapuram@EPINHYDW1124.princeton.epam.com>
2019-05-22 17:43:48 +0530

    Update on MentorStudentInfoServiceImpl

--- src/main/java/com/epam/services/mentor/MentorStudentInfoServiceImpl.java
+++ src/main/java/com/epam/services/mentor/MentorStudentInfoServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
+import com.epam.dao.admin.StudentBean;
 import com.epam.dao.mentor.MentorStudent;
 
 public class MentorStudentInfoServiceImpl implements MentorStudentInfoService {
@@ -45,8 +46,54 @@ public class MentorStudentInfoServiceImpl implements MentorStudentInfoService {
 	return studentList;
 	}
 	@Override
-	public List<MentorStudent> mentorViewStudentDetails(Connection con, String emailId) {
+	public List<StudentBean> mentorViewStudentDetails(Connection con, String emailId) {
 		
-		return null;
+		List<StudentBean> studentList = new ArrayList<>();
+		String sql = "call viewStudent('"+emailId+"');";
+		try(CallableStatement cs= con.prepareCall(sql);)
+		{
+			try(ResultSet rs = cs.executeQuery();)
+			{
+				while(rs.next())
+				{
+					StudentBean student=new StudentBean();
+					student.setFirstName(rs.getString("first_name"));
+					student.setLastName(rs.getString("last_name"));
+					student.setDob(rs.getDate("date_of_birth")); 
+					student.setEmail(rs.getString("email_id"));
+					student.setGender(rs.getString("gender"));
+					student.setContactNumber(rs.getLong("contact"));
+					student.setPersonalLocation(rs.getString("location"));
+					student.setCollegeName(rs.getString("college_name"));
+					student.setCollegeLocation(rs.getString("college_loc"));
+					student.setGraduation(rs.getString("graduation"));
+					student.setGraduationSpeciality(rs.getString("graduation_stream"));
+					student.setYearOfPassedOut(rs.getInt("Passed_out_year"));
+					student.setGraduationMarks(rs.getInt("Graduation_Marks"));
+					student.setTwelveth(rs.getInt("Inter_Marks"));
+					student.setTenth(rs.getInt("SSc_Marks"));
+					student.setBatchId(rs.getString("Batch_id"));
+					student.setEmployeeType(rs.getString("Emp_type"));
+					student.setCoreSkill(rs.getString("core_skill"));
+					student.setPreferredStudentStream(rs.getString("Preferred_Student_Stream"));
+					student.setAssignedStream(rs.getString("Assigned_Stream"));
+					student.setDateOfJoining(rs.getDate("Date_Of_Joining"));
+					student.setMentorName(rs.getString("mentor_name"));
+					student.setAssignedLocation(rs.getString("Assigned_location"));
+					student.setRelocation(rs.getString("relocation"));
+					student.setStatus(rs.getString("status"));
+					studentList.add(student);
+				}
+			}
+
+		}
+		catch(Exception e)
+		{
+			studentList=null;
+			LOGGER.debug(e.getMessage());
+		}
+		
+	return studentList;
+		
 	}
 }
--- src/main/java/com/epam/servlets/mentor/MentorStudentActionServlet.java
+++ src/main/java/com/epam/servlets/mentor/MentorStudentActionServlet.java
@@ -1,11 +1,25 @@
 package com.epam.servlets.mentor;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import com.epam.dao.admin.StudentBean;
+import com.epam.dao.mentor.MentorStudent;
+import com.epam.services.login.Menu;
+import com.epam.services.login.MenuItemsSingleton;
+import com.epam.services.mentor.MentorStudentInfoService;
+import com.epam.services.mentor.MentorStudentInfoServiceImpl;
+import com.epam.utils.ConstantsUtility;
+import com.epam.utils.DBManager;
 
 /**
  * Servlet implementation class MentorStudentActionServlet
@@ -13,17 +27,43 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/MentorStudentActionServlet")
 public class MentorStudentActionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	 private static final MentorStudentInfoService studentInfoService = new MentorStudentInfoServiceImpl();
+	  private static final Logger LOGGER = Logger.getLogger( MentorStudentActionServlet.class); 
+
        
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+
 		String actionView=request.getParameter("actionView");
-		String emailId=request.getParameter("email_id");
+		String studentEmailId=request.getParameter("email_id");
+		List<StudentBean> studentList =null;
+		String pageUrl=null;
 		
 		if(actionView.equalsIgnoreCase("View"))
 		{
-			//MentorStudentViewService mentorStudentViewService=new MentorStudentViewServiceImpl();
-			request.setAttribute("student", "Demo");
-			request.getRequestDispatcher("/mentor/Mentor_ViewStudentDetail.jsp").forward(request, response);
+			
+			
+			try(Connection con=DBManager.getConnection();)
+			{
+				studentList=studentInfoService.mentorViewStudentDetails(con, studentEmailId);
+				pageUrl=request.getServletContext().getInitParameter(ConstantsUtility.RESULT_PAGE_FOR_MENTOR_VIEW_STUDENT_INFO);
+				request.setAttribute("students", studentList);
+				List<Menu> menuList=MenuItemsSingleton.getInstance().getMenuItems();
+				request.setAttribute(ConstantsUtility.MENU_LIST, menuList);
+				request.setAttribute("pageState", "STUDENT INFO");
+				request.getRequestDispatcher(pageUrl).forward(request, response);
+
+			}
+			catch(Exception e)
+			{
+				pageUrl=request.getServletContext().getInitParameter(ConstantsUtility.ERROR_PAGE);
+				request.setAttribute("errorMsg", e.getMessage());
+				request.getRequestDispatcher(pageUrl).forward(request, response);
+				LOGGER.error("Exception occured in MentorStudentInfo = {}", e);
+			}
+		
+			LOGGER.debug("Exit from servlet");
 		}
 		
 	}
